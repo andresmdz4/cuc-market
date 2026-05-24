b5fb32c36744097c9af9ce47a7730203bd463a9a
Camilo_Velandia <mvcamilo982@gmail.com>
2026-05-24 18:39:30 -0500

    admin delete users

--- cuc-market/server/routes/adminRoutes.js
+++ cuc-market/server/routes/adminRoutes.js
@@ -2,9 +2,11 @@ import express from "express"
 
 import User from "../models/User.js"
 import Product from "../models/Product.js"
+
 import {
   protect
 } from "../middleware/authMiddleware.js"
+
 import adminMiddleware from "../middleware/adminMiddleware.js"
 
 const router = express.Router()
@@ -15,6 +17,8 @@ const router = express.Router()
 
 router.get(
   "/users",
+  protect,
+  adminMiddleware,
   async (req, res) => {
 
     try {
@@ -42,6 +46,8 @@ router.get(
 
 router.get(
   "/products",
+  protect,
+  adminMiddleware,
   async (req, res) => {
 
     try {
@@ -73,6 +79,8 @@ router.get(
 
 router.delete(
   "/product/:id",
+  protect,
+  adminMiddleware,
   async (req, res) => {
 
     try {
@@ -103,6 +111,82 @@ router.delete(
       res.status(500).json({
         message:
           "Error eliminando producto"
+      })
+
+    }
+
+  }
+)
+
+// =====================================
+// DELETE USER
+// =====================================
+
+router.delete(
+  "/user/:id",
+  protect,
+  adminMiddleware,
+  async (req, res) => {
+
+    try {
+
+      // NO ELIMINARSE A SI MISMO
+      if (
+
+        req.user._id.toString() ===
+        req.params.id
+
+      ) {
+
+        return res.status(400).json({
+
+          message:
+            "No puedes eliminarte a ti mismo"
+
+        })
+
+      }
+
+      const user =
+        await User.findById(
+          req.params.id
+        )
+
+      if (!user) {
+
+        return res.status(404).json({
+
+          message:
+            "Usuario no encontrado"
+
+        })
+
+      }
+
+      // ELIMINAR PRODUCTOS
+      await Product.deleteMany({
+
+        user: user._id
+
+      })
+
+      // ELIMINAR USER
+      await user.deleteOne()
+
+      res.json({
+
+        message:
+          "Usuario eliminado"
+
+      })
+
+    } catch (error) {
+
+      res.status(500).json({
+
+        message:
+          "Error eliminando usuario"
+
       })
 
     }
